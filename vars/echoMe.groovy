6ee611c4752d8466001b7fc91b69ce7edd034bae
Steve Boardwell <steve.boardwell@datameer.com>
2020-07-09 16:34:02 +0200

    Update echoMe.groovy

--- vars/echoMe.groovy
+++ vars/echoMe.groovy
@@ -1,5 +1,20 @@
-def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    echo "Hello, ${name}."
+#!/usr/bin/groovy
+
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def timeoutTime = config.timeoutTime ?: 24
+    def proceedMessage = """Would you like to promote version ${config.version} to the next environment?"""
+
+    try {
+        timeout(time: timeoutTime, unit: 'HOURS') {
+            echo "$proceedMessage"
+        }
+    } catch (err) {
+        throw err
+    }
 }
